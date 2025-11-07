68a2aafd563dea97923de9d6110e4c5bd33f0ae4
Kexuan Miao <113741677+Kiyuu-o@users.noreply.github.com>
2025-11-07 11:06:59 -0700

    Update template_cf.cpp

--- template_cf.cpp
+++ template_cf.cpp
@@ -1,15 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define cmax(u, v) u = max(u, v)
+#define cmin(u, v) u = min(u, v)
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
+#define endl '\n';
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+const ll m = 1e9 + 7;
+const int MAXN = 1e6 + 1;
 
 void solve(){
-
+	
 }
 
 int main() {
